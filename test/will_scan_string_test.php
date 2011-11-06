2b874f6d9a755e46f0c2f3dd77a5b7ef7b8b2240
Toby Hinloopen <toby@kutcomputers.nl>
2011-11-06 14:51:31 +0100

    Added more tests

--- test/will_scan_string_test.php
+++ test/will_scan_string_test.php
@@ -6,15 +6,14 @@ require_once "will_scan_string.php";
 frseql("should perform a regular replacement", function()
 {
 	$ss = new WillScanString();
-	$ss->register_preg_replacement_callback("/:\\)/", function()
-	{ return "HAPPY"; });
+	$ss->register_replacement(":)", "HAPPY");
 	return $ss->replace(":)");
 }, "HAPPY");
 
 frseql("should update regular expressions with back references", function()
 {
 	$ss = new WillScanString();
-	$ss->register_preg_replacement_callback("/<([a-z]+)>.*?<\\/\\1>/", function($_, $tagname)
+	$ss->register_preg_replacement("/<([a-z]+)>.*?<\\/\\1>/", function($_, $tagname)
 	{ return $tagname; });
 	return $ss->get_replacement_pattern();
 }, "/(?:(<([a-z]+)>.*?<\\/\\2>))/");
@@ -22,9 +21,35 @@ frseql("should update regular expressions with back references", function()
 frseql("should perform a regexp replacement with a capture group", function()
 {
 	$ss = new WillScanString();
-	$ss->register_preg_replacement_callback("/<([a-z]+)>.*?<\\/\\1>/", function($_, $tagname)
+	$ss->register_preg_replacement("/<([a-z]+)>.*?<\\/\\1>/", function($_, $tagname)
 	{ return $tagname; });
 	return $ss->replace("<strong>hi!</strong>");
 }, "strong");
+
+frseql("should not replace replaced strings", function()
+{
+	$ss = new WillScanString();
+	$ss->register_replacement(":)", '<img src="happy.png" alt=":)" title=":)">');
+	$ss->register_replacement("<", "&lt;");
+	$ss->register_replacement(">", "&gt;");
+	$ss->register_replacement("\"", "&quot;");
+	$ss->register_replacement("&", "&amp;");
+	return $ss->replace("& :)");
+}, '&amp; <img src="happy.png" alt=":)" title=":)">');
+
+frseql("should be able to use multiple regular expressions to replace with", function()
+{
+	$ss = new WillScanString();
+	$ss->register_preg_replacement("/(a)(b)/", "AB");
+	$ss->register_preg_replacement("/(c)(d)/", "CD");
+	return $ss->replace("abcd");
+}, "ABCD");
+
+frseql("should be able to match newlines", function()
+{
+	$ss = new WillScanString();
+	$ss->register_replacement("\r\n", "<br>");
+	return $ss->replace("<br>");
+}, "<br>");
 
 ?>
