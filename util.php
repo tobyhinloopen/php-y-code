5face481ea819470b4139277b4523c60f64eed45
Toby Hinloopen <toby@kutcomputers.nl>
2011-11-06 15:42:54 +0100

    Added missing return-statement in html-helper

--- util.php
+++ util.php
@@ -4,6 +4,6 @@ function array_peek( $array )
 { return $array[count($array)-1]; }
 
 function utf8_html_entities( $string )
-{ htmlentities($string, ENT_COMPAT, "UTF-8"); }
+{ return htmlentities($string, ENT_COMPAT, "UTF-8"); }
 
 ?>
