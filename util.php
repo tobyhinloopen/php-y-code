c5196cfbca28b9fb7c9ed1488820ff1c231f6702
Toby Hinloopen <toby@kutcomputers.nl>
2011-11-06 15:34:37 +0100

    Added html escaping function to utils

--- util.php
+++ util.php
@@ -1,7 +1,9 @@
 <?php
 
-function array_peek($array) {
-	return $array[count($array)-1];
-}
+function array_peek( $array )
+{ return $array[count($array)-1]; }
+
+function utf8_html_entities( $string )
+{ htmlentities($string, ENT_COMPAT, "UTF-8"); }
 
 ?>
