7a2dd13b2a889118ba644954a0426fc91c107ac7
Toby Hinloopen <toby@kutcomputers.nl>
2011-11-06 14:39:33 +0100

    added some tests to cover some bugs

--- test/will_scan_string_test.php
+++ test/will_scan_string_test.php
@@ -11,12 +11,19 @@ frseql("should perform a regular replacement", function()
 	return $ss->replace(":)");
 }, "HAPPY");
 
+frseql("should update regular expressions with back references", function()
+{
+	$ss = new WillScanString();
+	$ss->register_preg_replacement_callback("/<([a-z]+)>.*?<\\/\\1>/", function($_, $tagname)
+	{ return $tagname; });
+	return $ss->get_replacement_pattern();
+}, "/(?:(<([a-z]+)>.*?<\\/\\2>))/");
+
 frseql("should perform a regexp replacement with a capture group", function()
 {
 	$ss = new WillScanString();
 	$ss->register_preg_replacement_callback("/<([a-z]+)>.*?<\\/\\1>/", function($_, $tagname)
 	{ return $tagname; });
-	die($ss->get_replacement_pattern());
 	return $ss->replace("<strong>hi!</strong>");
 }, "strong");
 
