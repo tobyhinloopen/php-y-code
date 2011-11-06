4bfb8fb7e7bfceefd204d8337e273ed48d2b02c5
Toby Hinloopen <toby@kutcomputers.nl>
2011-11-06 15:36:54 +0100

    Implemented some basics in ycode

--- ycode.php
+++ ycode.php
@@ -2,9 +2,36 @@
 
 require_once "will_scan_string.php";
 
-class Ycode
+$ycode_global_string_scanner = new WillScanString();
+$ycode_nested_string_scanner = new WillScanString();
+
+$ycode_global_string_scanner->register_preg_replacement("/(?:\\A|\\^(\\d))([\\s\\S]*?)(?=\\Z|\\^\\d)/", function($_, $color, $content)
 {
-	
+	global $ycode_nested_string_scanner;
+	return sprintf('<span class="color color%d">%s</span>', intval($color), $ycode_nested_string_scanner->replace($content));
+});
+
+foreach(array(
+	"/^\\/\\/\\s*(.+)/" => function($_, $username)
+	{ return sprintf('<a href="user_by_username.php?username=%s">%s</a>', utf8_html_entities(rawurlencode($username)), utf8_html_entities($username)); },
+	"/~~~~\\r\\n(.+?)\\r\\n([\\s\\S]*?)\\r\\n~~~~/" => function($_, $quotee, $content)
+	{ global $ycode_global_string_scanner;
+	  return sprintf('<blockquote><span class="User Name">%s</span>%s</blockquote>', utf8_html_entities($quotee), $ycode_global_string_scanner->replace($content)); },
+	"/</" => "&lt;",
+	"/>/" => "&gt;",
+	"/&/" => "&amp;",
+	"/(?:\\r\\n|\\r|\\n)/" => function($_)
+	{ return "<br>"; }
+) as $pattern => $callback)
+{
+	$ycode_global_string_scanner->register_preg_replacement($pattern, $callback);
+	$ycode_nested_string_scanner->register_preg_replacement($pattern, $callback);
+}
+
+function parse_ycode_formatted_string_to_html( $string )
+{
+	global $ycode_global_string_scanner;
+	return $ycode_global_string_scanner->replace($string);
 }
 
 ?>
