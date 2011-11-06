46aafeb311452243cbfdf6bfaf549ce68195ee33
Toby Hinloopen <toby@kutcomputers.nl>
2011-11-06 14:10:54 +0100

    Made get_capture_groups work

--- regexp_traits.php
+++ regexp_traits.php
@@ -2,7 +2,22 @@
 
 function get_capture_groups( $regular_expression )
 {
-	
+	$capture_group_pattern = "/(?<!\\\\)\\((?:\\?(?:<([a-z]+)\\>|'([a-z]+)')|(?!\\?))/i";
+	$matches = array();
+	preg_match_all($capture_group_pattern, $regular_expression, $matches, PREG_SET_ORDER);
+	$groups = array();
+	$c = 1;
+	foreach($matches as $match)
+	{
+		if($match[1] != "")
+			$name = $match[1];
+		elseif($match[2] != "")
+			$name = $match[2];
+		else
+			$name = $c++;
+		array_push($groups, $name);
+	}
+	return $groups;
 }
 
 ?>
